8ee28ea8f44a324261fe805d843d18d5367cc833
Lina <linariddersporre@gmail.com>
2022-02-10 11:36:45 +0100

    add tags

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 const url = `https://scoreboard-ed666-default-rtdb.europe-west1.firebasedatabase.app/.json`;
 
-const username = $(section)
+const username = document.querySelector('.userName');
+const addButton = document.querySelector('.addButton');
+const playerName = document.querySelector('.playerName');
+
+let userInput = '';
+addButton.addEventListener('click', function(event){
+    userInput = username.value;
+    console.log(userInput);
+    playerName.innerText = userInput;
+    event.preventDefault();
+});
+
