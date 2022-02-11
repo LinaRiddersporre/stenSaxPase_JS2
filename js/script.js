e56f981ee46b68ec6fd6b8e4bdd10828d40603da
Lina <linariddersporre@gmail.com>
2022-02-11 14:04:21 +0100

    functional programming

--- js/script.js
+++ js/script.js
@@ -1,79 +1,65 @@
 const url = `https://scoreboard-ed666-default-rtdb.europe-west1.firebasedatabase.app/.json`;
 
-const username = document.querySelector('.userName');
-const addButton = document.querySelector('.addButton');
-const playerName = document.querySelector('.playerName');
 
-let userInput = '';
-addButton.addEventListener('click', function(event){
-    userInput = username.value;
+document.querySelector('.addButton').addEventListener('click', function(event){
+    userInput = document.querySelector('.userName').value;
     console.log(userInput);
-    playerName.innerText = userInput;
+    document.querySelector('.playerName').innerText = userInput;
     event.preventDefault();
 });
 
-const computerChoice = document.querySelector('.computerChoice');
+
 function computerRandomChoice(){
     let randomNumber = Math.floor(Math.random()*3);
     console.log('computerRandomChoice return: ', randomNumber);
     return randomNumber;
 }
 
-const stone = document.querySelector('.stone');
-const scissor = document.querySelector('.scissor');
-const bag = document.querySelector('.bag');
-const userParagrafScore = document.querySelector('.userScore');
-let computerScore = document.querySelector('.computerScore');
-let userPicChoice = document.querySelector('.userPicChoice');
-let computerPicChoice = document.querySelector('.computerPicChoice');
-
-
-let userChoice = document.querySelector('pictureOfChoice');
 let userScore = 0;
 
-stone.addEventListener('click', function(){
-    userPicChoice.src = 'img/sten.png';
+document.querySelector('.stone').addEventListener('click', function(){
+    document.querySelector('.userPicChoice').src = 'img/sten.png';
     let randomNumber = computerRandomChoice();
     if(randomNumber === 1){
         userScore++;
-        computerPicChoice.src = 'img/sax2.png';
+        document.querySelector('.computerPicChoice').src = 'img/sax2.png';
     }else if(randomNumber === 2){
-        computerPicChoice.src = 'img/påse.png';
-        computerScore.innerText = 'Spelet slut, försök igen';
+        document.querySelector('.computerPicChoice').src = 'img/påse.png';
+        document.querySelector('.computerScore').innerText = 'Spelet slut, försök igen';
     }else {
-        computerPicChoice.src = 'img/sten.png';
+        document.querySelector('.computerPicChoice').src = 'img/sten.png';
     }
-    userParagrafScore.innerText = userScore;
+    document.querySelector('.userScore').innerText = userScore;
 })
 
-scissor.addEventListener('click', function(){
-    userPicChoice.src = 'img/sax2.png';
+document.querySelector('.scissor').addEventListener('click', function(){
+    document.querySelector('.userPicChoice').src = 'img/sax2.png';
     let randomNumber = computerRandomChoice();
     if(randomNumber === 2){
         userScore++;
-        computerPicChoice.src = 'img/påse.png';
+        document.querySelector('.computerPicChoice').src = 'img/påse.png';
     }else if(randomNumber === 0){
-        computerPicChoice.src = 'img/sten.png';
-        computerScore.innerText = 'Spelet slut, försök igen';
+        document.querySelector('.computerPicChoice').src = 'img/sten.png';
+        document.querySelector('.computerScore').innerText = 'Spelet slut, försök igen';
     }else{
-        computerPicChoice.src = 'img/sax2.png';
+        document.querySelector('.computerPicChoice').src = 'img/sax2.png';
     }
-    userParagrafScore.innerText = userScore;
+    document.querySelector('.userScore').innerText = userScore;
 
 })
 
-bag.addEventListener('click', function(){
-    userPicChoice.src = 'img/påse.png';
+document.querySelector('.bag').addEventListener('click', function(){
+    document.querySelector('.userPicChoice').src = 'img/påse.png';
     let randomNumber = computerRandomChoice();
     if(randomNumber === 0){
         userScore++;
-        computerPicChoice.src = 'img/sten.png';
+        document.querySelector('.computerPicChoice').src = 'img/sten.png';
     }else if(randomNumber === 1){
-        computerPicChoice.src = 'img/sax2.png';
-        computerScore.innerText = 'Spelet slut, försök igen';
+        document.querySelector('.computerPicChoice').src = 'img/sax2.png';
+        document.querySelector('.computerScore').innerText = 'Spelet slut, försök igen';
     }else{
-        computerPicChoice.src = 'img/påse.png';
+        document.querySelector('.computerPicChoice').src = 'img/påse.png';
     }
-    userParagrafScore.innerText = userScore;
+    document.querySelector('.userScore').innerText = userScore;
 
 })
